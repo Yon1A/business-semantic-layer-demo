819923dfcbbb188285a454214cc4d5fbf0e4d46f
Yoni Adler <yoni.adler@Mac-2.lan>
2026-06-07 20:57:28 +0300

    Refresh demo after review gate fixes

---
+++ assets/useResourceList-5XNcAFJH.js
@@ -0,0 +1,1 @@
+import{e as r,n as s}from"./index-YjLxhSWp.js";function o(e,u){return r({queryKey:["resource-list",e.route,!1],queryFn:()=>s(e.route)})}export{o as u};
--- assets/useResourceList-BVbFP3G0.js
+++
@@ -1,1 +0,0 @@
-import{e as r,n as s}from"./index-D82r6rZK.js";function o(e,u){return r({queryKey:["resource-list",e.route,!1],queryFn:()=>s(e.route)})}export{o as u};
